52079b9f1e6aab3478f33258bbce21801cad86d0
ckdudgus <c91878074@gmail.com>
2022-03-31 17:32:28 +0900

    라우팅 연결 성공

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+
+var getsend = require('./api/getsend')
+
+
+app.use('/getsend',getsend)
 app.set('port' , 8080)
 app.get('/' , (req , res) => {
     res.send('노드연결성공')
